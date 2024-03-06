6b25542790cc95a007782776607e05433efa9797
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-03-06 18:04:24 +0500

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,22 +11,30 @@ const Dashboard = (props: Props) => {
   return (
     <Layout>
       <div className="relative h-screen">
-        <div className=" py-11 px-16 ">
-          <div className=" relative">
-            <div className="absolute hidden sm:flex  w-full ">
+        <nav className=" py-11 px-16 ">
+          <div>
+            <div className=" relative">
               <Image
-                className="w-[70px] lg:w-[90px] xl:w-[129px]"
+                className="fixed hidden sm:flex w-[70px] lg:w-[90px] xl:w-[129px]"
                 alt=""
                 width={129}
                 height={60}
                 src={"/Assets/ZDoc.svg"}
               />
+              <Image
+                className=" w-[120px] absolute hidden left-[50%] right-[50%]  md:flex items-end justify-center lg:w-[143px] xl:w-[183px]"
+                alt=""
+                width={204}
+                height={50}
+                src={"/Assets/avav.svg"}
+              />
+              <div className="flex items-center gap-4 md:gap-6 lg:gap-10 absolute right-0">
+                <LocationDateFlag />
+                <ProfileButton />
+              </div>
             </div>
-            <div className="flex items-center gap-4 md:gap-6 lg:gap-10 absolute right-0">
-              <LocationDateFlag />
-              <ProfileButton />
-            </div>
-            <div className="absolute top-12 right-0">
+
+            <div className="grid gap-4 justify-end pt-12">
               <Image
                 className="w-[143px] md:w-[183px]"
                 alt=""
@@ -34,21 +42,12 @@ const Dashboard = (props: Props) => {
                 height={40}
                 src={"/Assets/zima.svg"}
               />
-            </div>
-            <h1 className="absolute top-28 right-0 animate-pulse-slow">
-              try for free
-            </h1>
-            <div className="absolute hidden  md:flex justify-center w-full">
-              <Image
-                className=" w-[120px]  lg:w-[143px] xl:w-[183px]"
-                alt=""
-                width={204}
-                height={50}
-                src={"/Assets/avav.svg"}
-              />
+              <h1 className="flex justify-end animate-pulse-slow">
+                try for free
+              </h1>
             </div>
           </div>
-        </div>
+        </nav>
         <Image
           className=" absolute bottom-0 right-0 w-5/6 sm:w-[510px] xl:w-[520px] 2xl:w-[541px]"
           alt=""
